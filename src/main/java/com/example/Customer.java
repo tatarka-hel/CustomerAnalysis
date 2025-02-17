54139c6b145b6a577d661629e8a13d5615dc241a
tatarka-hel <milan.tatarkovic@tesco.com>
2025-02-17 21:51:42 +0100

    Initial state

--- src/main/java/com/example/Customer.java
+++ src/main/java/com/example/Customer.java
@@ -1,5 +1,12 @@
 package com.example;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Customer {
     private final String UUID;
     private String data;
@@ -18,5 +25,34 @@ public class Customer {
     public void process(){
         data = "Analysis output";
         isProcessed=true;
+    }
+
+    public static List<Customer> loadCustomers(String path){
+
+        List<Customer> customers=null;
+        Path file = Path.of("input.csv");
+        try (Stream<String> lines = Files.lines(file)) {
+            customers = lines.map(line -> {
+                Customer c = new Customer(line);
+                c.process();
+                return c;
+            }).toList();
+        } catch (IOException e) {
+            System.out.println("Failed to load the file");
+        }
+        return customers;
+    }
+
+    public static void saveCustomers(List<Customer> customers, String path){
+
+        String header = "UUID,data";
+        try (PrintWriter writer = new PrintWriter(path)) {
+            writer.println(header);
+            for(Customer c : customers){
+                writer.println(c);
+            }
+        } catch (IOException e) {
+            System.out.println("Failed to save the file");
+        }
     }
 }
--- src/main/java/com/example/CustomerAnalysisCommand.java
+++ src/main/java/com/example/CustomerAnalysisCommand.java
@@ -4,11 +4,7 @@ import io.micronaut.configuration.picocli.PicocliRunner;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.List;
-import java.util.stream.Stream;
 
 @Command(name = "CustomerAnalysis", description = "...",
         mixinStandardHelpOptions = true)
@@ -20,22 +16,10 @@ public class CustomerAnalysisCommand implements Runnable {
     public static void main(String[] args) throws Exception {
         PicocliRunner.run(CustomerAnalysisCommand.class, args);
 
-        List<Customer> customers = null;
-
-        //process input
-        Path file = Path.of("input.csv");
-        try (Stream<String> lines = Files.lines(file)) {
-            customers = lines.map(line -> {
-                Customer c = new Customer(line);
-                c.process();
-                return c;
-            }).toList();
-        } catch (IOException e) {
-            System.out.println("Failed to load the input");
-        }
-
-
+        List<Customer> customers = Customer.loadCustomers("input.csv");
         System.out.println(customers);
+
+        Customer.saveCustomers(customers,"output.csv");
     }
 
     public void run() {
